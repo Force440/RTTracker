0b8fa51feb2a9204422de170c5843121b65adcdb
Force440 <drjsavino@gmail.com>
2026-08-27 23:41:07 -0500

    Support multiple receipt pages per test

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rt-tracker-pwa-v1.1.0';
+const CACHE='rt-tracker-pwa-v1.2.0';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./core.js','./manifest.webmanifest','./seed-data.json','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
